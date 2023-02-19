51e4b94b2e5bf3d4f14a0159327d1569699dbe45
rdhlaura <laurarodriguezdeharo@outlook.es>
2023-02-19 21:39:19 +0100

    gh-pages commit

---
+++ dist/assets/contacto-84b15553.js
@@ -0,0 +1,1 @@
+import{_ as t,o,c}from"./index-ef2b0b28.js";const e={name:"ContactPage"};function a(n,r,s,p,_,f){return o(),c("h1",null,"pagina de contacto")}const d=t(e,[["render",a]]);export{d as default};
